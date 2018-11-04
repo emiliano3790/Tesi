67ac708957f8a9f645f10445f43002d2418662b8
emiliano.fiorentini <emilianofiorentini@gmail.com>
2018-11-04 15:32:06 +0100

    Mosaicatura nelle bande B2, B3 e B4: a ogni banda corrisponde un mosaico differente

--- Main.py
+++ Main.py
@@ -9,38 +9,39 @@ from snappy import HashMap
 
 
 # input directory with all products
-input_dir = '/home/famiglia/Scrivania/Prova_Tesi/input_dir'
+partial_input_dir = '/home/famiglia/Scrivania/Tesi_Magistrale/Prova_Tesi/input_dir/r10m/'
 # output folder
-output_dir = '/home/famiglia/Scrivania/Prova_Tesi/output_dir'
+output_dir = '/home/famiglia/Scrivania/Tesi_Magistrale/Prova_Tesi/output_dir'
 
+arr_band = ['B2', 'B3', 'B4']
 parameters = HashMap()
-parameters.put('combine', 'OR')
-parameters.put('combine', 'OR')
+parameters.put('combine', 'AND')
+#parameters.put('combine', 'OR')
 parameters.put('resampling', 'Nearest')
 parameters.put('crs', 'EPSG:4326')
 parameters.put('westBound', '11.35495423')
 parameters.put('northBound', '42.44744303')
 parameters.put('eastBound', '13.91997284')
 parameters.put('southBound', '41.40772619')
-parameters.put('pixelSizeX', 0.001)
-parameters.put('pixelSizeY', 0.001)
+parameters.put('pixelSizeX', 0.05)
+parameters.put('pixelSizeY', 0.05)
 Variable = jpy.get_type('org.esa.snap.core.gpf.common.MosaicOp$Variable')  # Ho richiamato il costruttore Variable
+#vars = jpy.array('org.esa.snap.core.gpf.common.MosaicOp$Variable', len(arr_band))
 vars = jpy.array('org.esa.snap.core.gpf.common.MosaicOp$Variable', 1)
-vars[0] = Variable('band_1', 'band_1')
-parameters.put('variables', vars)
+parameters.put('Region', 'Rectangle(0, 0 , 1000, 1000)')
+#Mosaic = GPF.createProduct('Subset', parameters, products)
 
-
-# creating file with input images
-products = []
-for product in os.listdir(input_dir):  # os.listdir restituisce una lista con tutti gli elementi contenuti nella cartella specificata
-    products.append(ProductIO.readProduct(os.path.join(input_dir, product)))
-# bnames = products[0].getBandNames()
-# print type(bnames)
-# print str(bnames[0])
-# band_1 = str(bnames[0])
-# parameters.put('variables.variable.name', band_1)
-
-# creating mosaic
-Mosaic = GPF.createProduct('Mosaic', parameters, products)
-# Writing output
-ProductIO.writeProduct(Mosaic, os.path.join(output_dir, 'out_file'), 'BEAM-DIMAP')
+for band in arr_band:
+    # creating file with input images
+    products = []
+    input_dir = partial_input_dir + band
+    print input_dir
+    for product in os.listdir(input_dir):  # os.listdir restituisce una lista con tutti gli elementi contenuti nella cartella specificata
+        products.append(ProductIO.readProduct(os.path.join(input_dir, product)))
+        vars[0] = Variable(band, 'band_1')
+        parameters.put('variables', vars)
+        # creating mosaic
+        Mosaic = GPF.createProduct('Mosaic', parameters, products)
+        # Writing output
+        out_file_name = 'out_file' + band
+        ProductIO.writeProduct(Mosaic, os.path.join(output_dir, out_file_name), 'BEAM-DIMAP')
